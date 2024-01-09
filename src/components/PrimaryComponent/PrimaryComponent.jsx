e8e439877286cc27f99815d3a7ba31ad8344c65a
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-10 02:20:54 +0600

    Add React toastify and PrimaryComponent to the project

---
+++ src/components/PrimaryComponent/PrimaryComponent.jsx
@@ -0,0 +1,35 @@
+// react router import
+import { RouterProvider } from "react-router-dom";
+
+// router
+import router from "../../router/router";
+
+// react toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide } from "react-toastify";
+
+const PrimaryComponent = () => {
+  return (
+    <div className="text-textPrimary font-default min-h-screen flex flex-col max-w-[120rem] mx-auto overflow-x-hidden">
+      {/* react toastify */}
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        transition={Slide}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+};
+
+export default PrimaryComponent;

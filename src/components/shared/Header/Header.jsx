1800757f64824da79796290daf20a45248faed35
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-10 15:14:38 +0600

    Change login button name in Header

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -42,7 +42,7 @@ const Header = ({ modifyClasses = "" }) => {
             )}
 
             {!appLoading && !profileData && (
-              <LinkBtn text="Sign In" url="/auth/login" />
+              <LinkBtn text="Employee Login" url="/auth/login" />
             )}
 
             {/* if app is finished loading and user is truthy, show the userprofile */}

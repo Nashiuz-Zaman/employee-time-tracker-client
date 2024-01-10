dbd9573711669a534cb2e406ea5c46f0479b89c0
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-10 18:32:40 +0600

    Add a file in the project for storing fixed ui data

---
+++ src/data/homeUiContent.jsx
@@ -0,0 +1,8 @@
+export const homeTopBannerTextContent = {
+  heading: (
+    <>
+      <span className="text-primary">Good Morning.</span>
+    </>
+  ),
+  description1: <>Ready to start your day at work?</>,
+};

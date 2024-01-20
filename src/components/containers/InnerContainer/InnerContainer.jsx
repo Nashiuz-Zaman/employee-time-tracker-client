1826dbe91cd1df85f33ff553090c61d067361684
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-20 15:00:47 +0600

    Modify InnerContainer inline padding

--- src/components/containers/InnerContainer/InnerContainer.jsx
+++ src/components/containers/InnerContainer/InnerContainer.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const InnerContainer = ({ children, modifyClasses = "" }) => {
   return (
     <div
-      className={`max-w-[90rem] 3xl:max-w-full px-3 xsm:px-6 md:px-8 lg:px-12 mx-auto 3xl:px-[7rem] ${modifyClasses}`}
+      className={`w-full px-3 xsm:px-6 md:px-8 lg:px-12 xl:px-[3.5rem] 2xl:px-[4.5rem] 3xl:px-[7rem] ${modifyClasses}`}
     >
       {children}
     </div>

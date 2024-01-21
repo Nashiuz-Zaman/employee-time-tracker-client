e48c6851302e1273e372f8f6fd07ca66f176c8d3
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-21 23:56:37 +0600

    Add Countdown component to ProgressTracker component and import related redux store states for it, also implement basic functionalites inside ProgressBtns component

--- src/components/pages/Home/ProgressTracker/ProgressTracker.jsx
+++ src/components/pages/Home/ProgressTracker/ProgressTracker.jsx
@@ -1,15 +1,34 @@
+// react
+import PropTypes from "prop-types";
+
 // components
 import ProgressBtns from "../../../shared/ProgressBtns/ProgressBtns";
+import Countdown from "../../../shared/Countdown/Countdown";
 
-const ProgressTracker = () => {
+// redux
+import { useSelector } from "react-redux";
+
+const ProgressTracker = ({ modifyClasses = "" }) => {
+  const { currentDuration } = useSelector((store) => store.workHours);
+
   return (
-    <div>
+    <div className={`${modifyClasses}`}>
+      {/* heading */}
       <h2 className="text-center text-3xl mb-8 font-semibold">
         Have a good day at work!
       </h2>
+
+      {/* progress buttons */}
       <ProgressBtns modifyClasses="mb-12" />
+
+      {/* countdown */}
+      <Countdown currentDuration={currentDuration} />
     </div>
   );
+};
+
+ProgressTracker.propTypes = {
+  modifyClasses: PropTypes.string,
 };
 
 export default ProgressTracker;
--- src/components/shared/ProgressBtns/ProgressBtns.jsx
+++ src/components/shared/ProgressBtns/ProgressBtns.jsx
@@ -1,17 +1,88 @@
 // react
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 
 // components
 import ButtonBtn from "./../ButtonBtn/ButtonBtn";
 
+// redux
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increaseDuration,
+  setWorking,
+  setCurrentDuration,
+} from "./../../../features/workHours/workHoursSlice";
+
 const ProgressBtns = ({ modifyClasses = "" }) => {
+  const dispatch = useDispatch();
+  const { currentDuration, working } = useSelector((store) => store.workHours);
+
+  console.log(currentDuration);
+
+  useEffect(() => {
+    const workingStorage = localStorage.getItem("working");
+
+    if (workingStorage === "yes") {
+      dispatch(setWorking(true));
+      const newTime = new Date();
+
+      const lastCurrentDuration = parseInt(
+        localStorage.getItem("currentDuration")
+      );
+      const lastTime = localStorage.getItem("lastTime");
+
+      const difference = (newTime - new Date(lastTime)) / 1000;
+
+      dispatch(setCurrentDuration(lastCurrentDuration + difference));
+    } else {
+      dispatch(setCurrentDuration(0));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    let timer;
+
+    if (working) {
+      timer = setTimeout(() => {
+        dispatch(increaseDuration());
+        clearTimeout(timer);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currentDuration, dispatch, working]);
+
+  const handleStart = () => {
+    dispatch(setWorking(true));
+  };
+
+  const handleEnd = () => {
+    dispatch(setWorking(false));
+    dispatch(setCurrentDuration(0));
+  };
+
+  const handlePause = () => {
+    dispatch(setWorking(false));
+    dispatch(setCurrentDuration(currentDuration));
+  };
+
   return (
     <div className={`flex justify-center items-center gap-4 ${modifyClasses}`}>
       {/* start/resume button */}
-      <ButtonBtn text={"Start Working"} colorTheme="black" />
+      <ButtonBtn
+        text={working ? "Take a break" : "Start Working"}
+        colorTheme="black"
+        onClickFunction={working ? handlePause : handleStart}
+      />
 
       {/* pause button */}
-      <ButtonBtn text={"Clock Out"} colorTheme="danger" />
+      <ButtonBtn
+        text={"Clock Out"}
+        colorTheme="danger"
+        onClickFunction={handleEnd}
+      />
     </div>
   );
 };

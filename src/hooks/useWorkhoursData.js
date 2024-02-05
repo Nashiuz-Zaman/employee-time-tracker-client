4ddc1f25c3356ba3fcef3023a1e7d3844934b2ea
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-05 23:54:13 +0600

    Refactor useWorkhourData hook

--- src/hooks/useWorkhoursData.js
+++ src/hooks/useWorkhoursData.js
@@ -4,22 +4,51 @@ import { useCallback } from "react";
 // hooks
 import useAxios from "./useAxios";
 
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { setWorkhourData } from "../features/workhour/workhourSlice";
+
 const useWorkhoursData = () => {
+  const dispatch = useDispatch();
+  const { profileData } = useSelector((store) => store.auth);
   const { axiosCustom } = useAxios();
 
   const createWorkhour = useCallback(
     async (employeeProfile) => {
-      const { _id, fullname, email, imageSource } = employeeProfile;
+      const { _id } = employeeProfile;
       const currentTime = new Date().toISOString();
-      const employeeData = { _id, fullname, email, imageSource, currentTime };
-
+      const employeeData = { _id, currentTime };
       const res = await axiosCustom.post("/workhours", employeeData);
-      console.log(res);
+      if (res.data.status === "success") {
+        return res.data.newWorkhour;
+      } else {
+        return false;
+      }
     },
     [axiosCustom]
   );
 
-  return { createWorkhour };
+  const handleClockIn = useCallback(async () => {
+    const newWorkhour = await createWorkhour(profileData);
+
+    if (!newWorkhour) {
+      return;
+    }
+
+    const workhourData = {
+      _id: newWorkhour._id,
+      working: true,
+      onBreak: false,
+      currentDuration: 0,
+      startedWorkAt: newWorkhour.startedWorkAt,
+      endedWorkAt: null,
+      currentDayDate: newWorkhour.currentDayDate,
+    };
+
+    dispatch(setWorkhourData(workhourData));
+  }, [createWorkhour, dispatch, profileData]);
+
+  return { handleClockIn };
 };
 
 export default useWorkhoursData;
